1af5a1a90e1251ffeb50b945d44d84b80db23751
sandiprayogo <prayogosandi67@gmail.com>
2022-07-28 21:24:37 +0700

    memperbaiki bug

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-
-/** todo Monica property */
-const arrColorMonica = ["Yellow", "Pink", "White", "Purple"];
-const arrRestoMonica = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"];
-
-let setColorMonica = new Set(arrColorMonica);
-let setRestoMonica = new Set(arrRestoMonica);
-
-const educationMonica = [
-  { name: "SD 01", city: "Jakarta", graduate: "2016" },
-  { name: "SMP 02", city: "Jakarta", graduate: "2019" },
-  { name: "SMA 03", city: "Tangerang" },
-];
-
-/** todo Sandi Property */
-const arrColorSandi = ["Blue", "White", "Red"];
-const arrRestoSandi = ["Sate", "Soto", "Sosis", "Kue", "Rujak", "Mie", "Kaldu", "Ramen", "Ote-ote"];
-
-let setColorSandi = new Set(arrColorSandi);
-let setRestoSandi = new Set(arrRestoSandi);
-
-const educationSandi = [
-  { name: "SDN 1", city: "Sumenep", graduate: "2007" },
-  { name: "MTsN 2", city: "Sumenep", graduate: "2013" },
-  { name: "MAN 1", city: "Sumenep", graduate: "2019" },
-  { name: "Universitas Trunojoyo", city: "Bangkalan" },
-];
-
-/** todo data input */
-
-//monica
 const firstUser = {
-  name: "Monica",
-  gender: "Female",
-  age: 17,
-  email: "monica@dingdong.com",
-  favoriteColor: [...setColorMonica],
+  name: "ANDIKA",
+  gender: "male",
+  age: 10,
+  email: "andika@dingdong.com",
+  favoriteColor: ["Yellow", "Pink", "White", "Purple"],
   isHavePet: "Yes",
-  education: educationMonica,
-  favoriteRestaurant: [...setRestoMonica],
+  education: [{ name: "SD 01", city: "Sumenep" }],
+  favoriteRestaurant: [
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Eggy",
+    "Tempura",
+    "Padang",
+    "Tteok",
+  ],
 };
-
-// sandi
 const secondUser = {
-  name: "Sandi Prayogo",
+  name: "SANDI",
   gender: "Male",
   age: 20,
   email: "sandi@dingdong.com",
-  favoriteColor: [...setColorSandi],
+  favoriteColor: ["Blue", "Black", "Red"],
   isHavePet: "No",
-  education: educationSandi,
-  favoriteRestaurant: [...setRestoSandi],
+  education: [
+    { name: "SDN 1", city: "Sumenep", graduate: "2013" },
+    { name: "MTsN", city: "Sumenep", graduate: "2016" },
+    { name: "MAN", city: "Sumenep", graduate: "2019" },
+    { name: "UTM", city: "Bangkalan" },
+  ],
+  favoriteRestaurant: [
+    "Tempura",
+    "Bento",
+    "Sushi",
+    "Padang",
+    "Katsu",
+    "Geprek",
+    "Pancake",
+    "Eggy",
+  ],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
-users.push(firstUser);
-users.push(secondUser);
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
